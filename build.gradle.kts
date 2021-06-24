4620200ef6be69f256cdeb79bca961abe9e34ea5
Zak <shearmanzak@gmail.com>
2021-06-25 00:45:56 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    java
+    `java-library`
     id("maven-publish")
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
@@ -10,15 +10,16 @@ repositories {
 
     // Used for query types
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
 }
 
-version = "1.2.0"
+version = "1.2.1"
 group = "pink.zak"
 
 dependencies {
-    implementation("com.google.guava:guava:30.1.1-jre")
-    implementation("com.google.code.gson:gson:2.8.7")
-    implementation("com.influxdb:influxdb-client-java:2.3.0")
+    api("com.google.guava:guava:30.1.1-jre")
+    api("com.google.code.gson:gson:2.8.7")
+    api("com.influxdb:influxdb-client-java:2.3.0")
 
     // Used for query types
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
